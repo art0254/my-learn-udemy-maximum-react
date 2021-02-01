e3948e8c2c7aafd3b39c33b51d79bfc06a89a320
art0254 <nana.ort@gmail.com>
2021-02-01 16:54:09 +0700

    set state by use prevState correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 import classes from "./App.css";
 import withClass from "../hoc/withClass";
-import Aux from '../hoc/Auxiliary';
+import Aux from "../hoc/Auxiliary";
 
 class App extends Component {
   state = {
@@ -16,6 +16,7 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false,
     showCockpit: true,
+    changeCounter: 0,
   };
 
   static getDrivedStateFromProps(props, state) {
@@ -52,7 +53,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props)=>{
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      }
+    });
   };
 
   deletePersonHandler = (personIndex) => {
