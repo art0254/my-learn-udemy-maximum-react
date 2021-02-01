4d0b794be453568149f13c812055b7deca21e1e1
art0254 <nana.ort@gmail.com>
2021-02-01 18:54:12 +0700

    refactor for easy vis function useContext & contextType

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,6 +10,7 @@ class Person extends Component {
     super(props);
     this.inputElementRef = React.createRef();
   }
+  static contextType = AuthContext;
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
@@ -18,11 +19,12 @@ class Person extends Component {
     console.log("[Person.js] rendering...");
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {(context) =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please Login</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please Login</p>
+        )}
+
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
