543e05248c59f6000c85ce315494b49bcba86dc4
art0254 <nana.ort@gmail.com>
2021-02-01 17:43:23 +0700

    use Ref in function components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,26 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     //Http Request
-    const timer = setTimeout(() => {
-      alert("Saved data to cloud!");
-    }, 1000);
+    toggleBtnRef.current.click();
+    // const timer = setTimeout(() => {
+    //   alert("Saved data to cloud!");
+    // }, 1000);
     return () => {
       console.log("[Cockpit.js] cleanup work in useEffect");
-      clearTimeout(timer);
+      //clearTimeout(timer);
     };
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("[Cockpit.js] 2nd useEffect");
 
     return () => {
-        console.log("[Cockpit.js] 2nd cleanup work in useEffect");
-      };
-  })
+      console.log("[Cockpit.js] 2nd cleanup work in useEffect");
+    };
+  });
 
   const assignedClasses = [];
   let btnClass = "";
@@ -37,7 +39,7 @@ const cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button  ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
