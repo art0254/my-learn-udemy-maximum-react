f81823cd5fe594fe4283917e70daaf22f04ba09c
art0254 <nana.ort@gmail.com>
2021-02-03 12:03:48 +0700

    add BuildController

--- buger_builder/src/components/Burger/Burger.js
+++ buger_builder/src/components/Burger/Burger.js
@@ -2,11 +2,18 @@ import React from "react";
 import classses from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingreditents).map((igKey) => {
-    return [...Array(props.ingreditents[igKey])].map((_, index) => {
-      return <BurgerIngredient key={igKey + index} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingreditents)
+    .map((igKey) => {
+      return [...Array(props.ingreditents[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingreditents!</p>
+    }
   return (
     <div className={classses.Burger}>
       <BurgerIngredient type="bread-top" />
