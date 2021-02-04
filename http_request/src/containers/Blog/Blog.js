c6b9947e8f1319492b890a41aec2a830e1e0c8f0
art0254 <nana.ort@gmail.com>
2021-02-04 14:29:40 +0700

    add display data on update

--- http_request/src/containers/Blog/Blog.js
+++ http_request/src/containers/Blog/Blog.js
@@ -8,10 +8,11 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null
   };
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      const posts = response.data;
+      const posts = response.data.slice(0,4);
       const updatedPosts = posts.map((post) => {
         return {
           ...post,
@@ -21,15 +22,19 @@ class Blog extends Component {
       this.setState({ posts: updatedPosts});
     });
   }
+
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id});
+  }
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return <Post key={post.id} title={post.title} author={post.author} clicked={()=> this.postSelectedHandler(post.id)}/>;
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
