8a824d3b31556aff46ae688d20de09074599a711
art0254 <nana.ort@gmail.com>
2021-02-05 18:47:24 +0700

    handler lazy loading (old version)

--- routing/src/App.js
+++ routing/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       // <BrowserRouter basename="my-app">
-      <BrowserRouter basename="my-app">
+      <BrowserRouter>
         <div className="App">
           <Blog />
         </div>
--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import FullPost from "./FullPost/FullPost";
+import asyncComponent from "../../hoc/asyncComponent";
 
 import "./Blog.css";
 
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 class Blog extends Component {
   render() {
     return (
@@ -40,7 +44,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} exact />
+          <Route path="/new-post" component={AsyncNewPost} exact />
           <Route path="/posts" component={Posts} />
           {/* <Redirect from="/" to="/posts" /> */}
           <Route render={() => <p>Not Found</p>} />
