5eb4761f8f361afbe6ef546fda4f477b204513a7
art0254 <nana.ort@gmail.com>
2021-02-27 21:15:43 +0700

    Using Utility Functions

--- advance_redux/src/store/reducers/counter.js
+++ advance_redux/src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 const initialState = {
   counter: 0,
 };
@@ -6,25 +7,21 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + 1,
-      };
+      });
     case actionTypes.DECREMENT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - 1,
-      };
+      });
     case actionTypes.ADD:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + action.val,
-      };
+      });
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - action.val,
-      };
+      });
   }
 
   return state;
--- advance_redux/src/store/reducers/result.js
+++ advance_redux/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 const initialState = {
   results: [],
 };
@@ -6,10 +7,9 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result }),
-      };
+      });
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
@@ -17,10 +17,9 @@ const reducer = (state = initialState, action) => {
       const updatedArray = state.results.filter(
         (result) => result.id !== action.resultElId
       );
-      return {
-        ...state,
+      return updateObject(state, {
         results: updatedArray,
-      };
+      });
   }
 
   return state;
