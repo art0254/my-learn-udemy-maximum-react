544df8477267c12aaedb42de747728433df4ffca
art0254 <nana.ort@gmail.com>
2021-03-01 23:41:57 +0700

    Storing the Token

--- buger_builder_authentication/src/containers/Auth/Auth.js
+++ buger_builder_authentication/src/containers/Auth/Auth.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
 class Auth extends Component {
   state = {
     controls: {
@@ -34,6 +36,7 @@ class Auth extends Component {
         touched: false,
       },
     },
+    isSignup: true,
   };
   checkValidity(value, rules) {
     let isValid = true;
@@ -71,6 +74,21 @@ class Auth extends Component {
     };
     this.setState({ controls: updatedControls });
   };
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignup
+    );
+  };
+
+  switchAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return { isSignup: !prevState.isSignup };
+    });
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -95,13 +113,21 @@ class Auth extends Component {
     ));
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
+        <Button btnType="Danger" clicked={this.switchAuthModeHandler}>
+          SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}
+        </Button>
       </div>
     );
   }
 }
-
-export default Auth;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password, isSignup) =>
+      dispatch(actions.auth(email, password, isSignup)),
+  };
+};
+export default connect(null, mapDispatchToProps)(Auth);
