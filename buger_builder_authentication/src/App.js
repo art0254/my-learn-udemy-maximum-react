bca56680d881df3d48a91d704289acd5ea516f82
art0254 <nana.ort@gmail.com>
2021-03-01 15:39:00 +0700

    Auth Form

--- buger_builder_authentication/src/App.js
+++ buger_builder_authentication/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import Layout from "./hoc/Layouts/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Order from "./containers/Orders/Orders";
-
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Layout from './hoc/Layouts/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Order from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <Switch>
+            <Route path="/auth" component={Auth} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Order} />
-            <Route path="/" component={BurgerBuilder}  />
+            <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
