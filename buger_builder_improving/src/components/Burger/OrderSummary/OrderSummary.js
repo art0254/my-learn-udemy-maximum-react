01bd250e15fe04586113b03d7d91cc9f523b66aa
art0254 <nana.ort@gmail.com>
2021-03-05 06:21:11 +0700

    Removing console.log()s

--- buger_builder_improving/src/components/Burger/OrderSummary/OrderSummary.js
+++ buger_builder_improving/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
-import Aux from "../../../hoc/Auxiliary/Auxiliary";
-import Button from "../../UI/Button/Button";
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Button from '../../UI/Button/Button';
 class OrderSummary extends Component {
   render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
-      return (
-        <li style={{ textTransform: "capitalize" }}>
-          <span>{igKey}</span> : {this.props.ingredients[igKey]}
-        </li>
-      );
-    });
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey} style={{ textTransform: 'capitalize' }}>
+            <span>{igKey}</span> : {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
     return (
       <Aux>
         <h3>Your Order</h3>
