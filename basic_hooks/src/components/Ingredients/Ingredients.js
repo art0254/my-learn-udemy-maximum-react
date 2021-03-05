373a8ab45c73b149cd12b86008f4f373a4c50271
art0254 <nana.ort@gmail.com>
2021-03-05 13:50:01 +0700

    useEffect() & Loading Data

--- basic_hooks/src/components/Ingredients/Ingredients.js
+++ basic_hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,11 +6,42 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      'https://react-hook-max-52acc-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
+
+  fetch(
+    'https://react-hook-max-52acc-default-rtdb.firebaseio.com/ingredients.json'
+  )
+    .then((response) => response.json())
+    .then((responseData) => {
+      const lodedIngredients = [];
+      for (const key in responseData) {
+        lodedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setUserIngredients(lodedIngredients);
+    });
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
